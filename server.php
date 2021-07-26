dec0470ae9946ecad031e4bbdc94686837f83ad2
worstfrontendcoder <omarsumon1030@gmail.com>
2021-07-26 18:04:17 -0500

    Additional Comments

--- server.php
+++ server.php
@@ -1,17 +1,22 @@
 <?php
 
+
 // code here:
 
+# An if satement that checks if:...
 if (isset($_POST['submit'])) {
+    // find user input
     $name = $_POST['name'];
     $mail = $_Post['mail'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-
-    $mailTo = "omarsumon1030@gmail.com";
+    
+    // variables for actual code
+    $mailTo = "youremail@email.com";
     $headers = "From: " . $mail;
     $txt = "You Have Gotten an Email From: " .$mail.".\n\n".$message;
 
+    // this does the stuff
     mail($mailTo, $subject, $txt, $headers);
     header("Location: contact.php?mailsend");
 }
